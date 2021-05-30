2e5e028852b2b07e372406bdabe8b722e729939b
akopilov <artemykopilov@gmail.com>
2021-05-30 20:00:28 +0300

    [#knan44] new controller added [dev, feature]

---
+++ src/main/java/ru/balacetracker/controller/TransactionAccountController.java
@@ -0,0 +1,14 @@
+package ru.balacetracker.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction-account")
+@AllArgsConstructor
+public class TransactionAccountController {
+
+
+
+}
